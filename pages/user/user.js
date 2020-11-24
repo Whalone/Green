d09659edf036e786493bda7d04b8560ba7936759
Whalone <469716638@qq.com>
2020-11-24 22:59:41 +0800

    1124

--- pages/user/user.js
+++ pages/user/user.js
@@ -10,7 +10,7 @@ Page({
   tabBar() {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
-        selected:3
+        selected:1
       })
     }
   },
