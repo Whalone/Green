b914296db393a618cffa17d7e700df1919f93555
Stefan.Liu <stefan.liu@oocl.com>
2021-04-14 14:04:01 +0800

    fix feed back page

--- pages/admin_feedBack/admin_feedBack.js
+++ pages/admin_feedBack/admin_feedBack.js
@@ -14,7 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.onShow();
   },
 
   /**
@@ -28,7 +28,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    util.request("/api/suggestion","","GET",(res)=>{
+    util.request("suggestion","","GET",(res)=>{
       this.setData({
         feedBack_list:res.data
       })
